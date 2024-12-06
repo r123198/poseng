824f3b33c2d36aa5a7a40383f41522a64d2ac314
r123198 <tibudan.rafael@gmail.com>
2024-12-07 04:11:59 +0800

    inital push

---
+++ android/app/src/main/kotlin/com/example/poseng/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.poseng
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PosengApp extends StatefulWidget {
+  const PosengApp({super.key});
+
+  @override
+  State<PosengApp> createState() => _PosengAppState();
+}
+
+class _PosengAppState extends State<PosengApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
